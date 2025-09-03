e3bffca64588651728b5a201c539dc8914887da3
amacharla15 <amacharla@csuchico.edu>
2025-09-03 09:07:29 -0700

    supervised, unsupervised, semisupervised learning theory

---
+++ supervisedlearning.py
@@ -0,0 +1,19 @@
+"""
+supervised learning: Classification and Regression:
+In supervised learning, the training set you feed to the algorithm includes the desired solutions, called labels
+
+The words target and label are generally treated as synonyms in supervised learning, but target is more common in regression tasks and label is more common in classification
+tasks.
+
+Unsupervised learning: Clustering & dimensionality reduction:
+ the training data is unlabeled. The system tries to learn without a teacher.
+
+ example, a car’s mileage may be strongly correlated with its age, so the dimensionality reduction algorithm will merge them into one feature that represents the car’s wear and tear. This is called feature extraction.
+
+ Hierarchical clustering is a type of clustering algorithm that builds a hierarchy of clusters. It is often represented as a tree-like diagram called a dendrogram, which shows the relationships between the clusters at different levels of granularity.
+
+ another common unsupervised task is association rule learning, in which the goal is to dig into large amounts of data and discover interesting relations between attributes.
+
+ 
+
+"""
